143100ed480a5bf42786ce22a9dfedce33c0489d
Pere <pere.solaclaver@gmail.com>
2020-10-27 01:50:16 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Login />
+			<Logout />
+			<Profile />
+		</div>
+	);
+}
+
+function Login() {
+	const { loginWithRedirect } = useAuth0();
+
+	return <button onClick={() => loginWithRedirect()}>Log In</button>;
+}
+
+function Logout() {
+	const { logout } = useAuth0();
+	return (
+		<button onClick={() => logout({ returnTo: window.location.origin })}>
+			Log Out
+		</button>
+	);
+}
+
+function Profile() {
+	const { user, isAuthenticated, isLoading } = useAuth0();
+
+	if (isLoading) {
+		return <div>Loading...</div>;
+	}
+
+	return (
+		isAuthenticated && (
+			<div>
+				<img src={user.picture} alt={user.name} />
+				<h2>{user.name}</h2>
+				<p>{user.email}</p>
+			</div>
+		)
+	);
 }
 
 export default App;
